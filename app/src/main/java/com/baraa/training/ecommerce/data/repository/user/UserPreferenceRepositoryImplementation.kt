38283b7c93af70b62aac3ea643aa9a335775595e
Baraa AbuAlrob <baraabualrub109@gmail.com>
2024-03-30 02:02:31 +0200

    implementation architecture and solid principles

---
+++ app/src/main/java/com/baraa/training/ecommerce/data/repository/user/UserPreferenceRepositoryImplementation.kt
@@ -0,0 +1,32 @@
+package com.baraa.training.ecommerce.data.repository.user
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import com.baraa.training.ecommerce.data.datasource.datastore.DataStoreKeys.IS_USER_LOGGED_IN
+import com.baraa.training.ecommerce.data.datasource.datastore.DataStoreKeys.USER_ID
+import com.baraa.training.ecommerce.data.datasource.datastore.dataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreferenceRepositoryImplementation(private val context: Context) : UserPreferenceRepository {
+
+    // Write to DataStore
+    override suspend fun saveLoginState(isLoggedIn: Boolean){
+        context.dataStore.edit { preferences ->
+            preferences[IS_USER_LOGGED_IN] = isLoggedIn
+        }
+    }
+
+    override suspend fun saveUserID(userId: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_ID] = userId
+        }
+    }
+
+    override suspend fun isUserLoggedIn(): Flow<Boolean> {
+        return context.dataStore.data.map { preferences ->
+            // Return the logged-in state, defaulting to false if not found.
+            preferences[IS_USER_LOGGED_IN] ?: false
+        }
+    }
+}
--- app/src/main/java/com/baraa/training/ecommerce/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/baraa/training/ecommerce/ui/login/viewmodel/LoginViewModel.kt
@@ -1,6 +1,9 @@
 package com.baraa.training.ecommerce.ui.login.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.baraa.training.ecommerce.data.repository.user.UserPreferenceRepository
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel (
+    val userPrefs: UserPreferenceRepository
+) : ViewModel() {
 }

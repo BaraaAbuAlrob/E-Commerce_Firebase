6f05600c10ef9df160d5de1a741c13abbeffe19d
Baraa AbuAlrob <b.abualrub3@student.aaup.edu>
2024-05-04 12:46:19 +0300

    Fix the interactive button spaces, and repair the edit text fileds (text and hint colors).
    Signed-off-by: Baraa AbuAlrob <b.abualrub3@student.aaup.edu>

--- app/src/main/java/com/baraa/training/ecommerce/ui/login/fragments/LoginFragment.kt
+++ app/src/main/java/com/baraa/training/ecommerce/ui/login/fragments/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.baraa.training.ecommerce.ui.login.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.baraa.training.ecommerce.R
 import com.baraa.training.ecommerce.data.repository.user.UserPreferenceRepositoryImplementation
 import com.baraa.training.ecommerce.databinding.FragmentLoginBinding
@@ -16,18 +19,65 @@ class LoginFragment : Fragment() {
         LoginViewModel(userPrefs = UserPreferenceRepositoryImplementation(requireActivity()))
     }
 
-    lateinit var binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        changeTheETFStrokeColors()
+    }
+
+    private fun changeTheETFStrokeColors() {
+        val emailLayout = binding.emailLayoutEdText
+        val emailEditText = binding.emailFiledEdText
+        val passwordLayout = binding.passwordLayoutEdText
+        val passwordEditText = binding.passwordFiledEdText
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed for this case
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed for this case
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    emailLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), R.color.primary_color)
+                } else {
+                    // Set the default stroke color when no text is entered
+                    emailLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), R.color.neutral_grey)
+                }
+            }
+        })
+
+        passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed for this case
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed for this case
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    passwordLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), R.color.primary_color)
+                } else {
+                    // Set the default stroke color when no text is entered
+                    passwordLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), R.color.neutral_grey)
+                }
+            }
+        })
     }
 
     companion object {

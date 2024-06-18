f8bb6f6e6be19a5cdcf5b5c91b4429671af11b5d
Baraa AbuAlrob <b.abualrub3@student.aaup.edu>
2024-06-18 17:17:04 +0300

    update gradle...
    Signed-off-by: Baraa AbuAlrob <b.abualrub3@student.aaup.edu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ buildscript {
 
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.1" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }

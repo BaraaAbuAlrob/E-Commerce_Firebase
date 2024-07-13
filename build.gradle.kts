1d9fedb258bd66923a88f67b2e230e2df503c3c0
Baraa AbuAlrob <b.abualrub3@student.aaup.edu>
2024-07-13 10:31:14 +0300

    Upgrade GPA from 8.5.0 to 8.5.1 and org.gradle.jvmargs from Xmx2048m to -Xmx3048m and add tryK2.
    Signed-off-by: Baraa AbuAlrob <b.abualrub3@student.aaup.edu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ buildscript {
 
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.1" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }

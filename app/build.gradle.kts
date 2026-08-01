760f6d63a9d706bf8256356428e53de64ff04009
Administrator <b.abualrub3@student.aaup.edu>
2026-08-01 22:31:28 +0300

    Update the dependencies.
    Signed-off-by: Administrator <b.abualrub3@student.aaup.edu>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id("com.android.legacy-kapt")
     id("kotlin-parcelize")
-    id("com.google.protobuf") version "0.9.4" apply true
+    id("com.google.protobuf") version "0.10.0" apply true
     id("com.google.dagger.hilt.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.baraa.training.ecommerce"
-    compileSdk = 34
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.baraa.training.ecommerce"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -60,69 +60,33 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 
     buildFeatures {
+        buildConfig = true
+        resValues = true
         dataBinding = true
         viewBinding = true
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.7"
+    packaging {
+        resources {
+            excludes += "/google/protobuf/*.proto"
+        }
     }
 }
 
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("androidx.activity:activity-ktx:1.9.3")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
-
-    // Datastore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-
-    // Splash Screen
-    implementation("androidx.core:core-splashscreen:1.0.1")
-
-    // compose
-    val composeBom = platform("androidx.compose:compose-bom:2024.10.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3:material3-window-size-class")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.28.0")
-    implementation("com.google.accompanist:accompanist-webview:0.32.0")
-    implementation("io.coil-kt:coil:2.6.0")
-    implementation("io.coil-kt:coil-compose:2.6.0")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.11")
-    implementation("androidx.fragment:fragment-compose:1.8.5")
-
     // Firebase dependencies
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-functions-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx:23.1.0")
-    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-functions")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.6.0")
 
     // Facebook auth dependency
-    implementation("com.facebook.android:facebook-android-sdk:16.0.0")
+    implementation("com.facebook.android:facebook-android-sdk:18.3.0")
 
     // third party libraries
     implementation("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
@@ -131,22 +95,22 @@ dependencies {
     implementation("com.facebook.shimmer:shimmer:0.5.0")
 
     // Glide
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    implementation("com.github.bumptech.glide:glide:5.0.9")
+    ksp("com.github.bumptech.glide:compiler:5.0.9")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.4.0")
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
-    kapt("com.google.dagger:hilt-android-compiler:2.47")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("com.google.dagger:hilt-android:2.60.1")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.4.0")
+    ksp("com.google.dagger:hilt-android-compiler:2.60.1")
+    ksp("androidx.hilt:hilt-compiler:1.4.0")
 
     // Navigation components
-    val navVersion = "2.8.3"
+    val navVersion = "2.9.8"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
@@ -155,24 +119,25 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
 
     // Lifecycle components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
 
     // Datastore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
 
     // Protobuf
-    implementation("com.google.protobuf:protobuf-kotlin-lite:4.26.0")
+    implementation("com.google.protobuf:protobuf-kotlin-lite:4.35.1")
+    implementation("com.google.protobuf:protobuf-javalite:4.35.1")
 
     // Material Design
-    implementation("com.google.android.material:material:1.12.0")
-    api("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.14.0")
+    api("com.google.android.material:material:1.14.0")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
 
 // Setup protobuf configuration, generating lite Java and Kotlin classes
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,11 @@ buildscript {
 
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("com.android.application") version "9.3.1" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.firebase.crashlytics") version "3.0.7" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("com.google.devtools.ksp") version "2.3.10" apply false
+    id("com.android.legacy-kapt") version "9.3.1" apply false
 }
